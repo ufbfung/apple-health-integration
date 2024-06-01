df0ef1edf4d63f60f4189e95fe10833b8812273c
ufbfung <ufbfung@gmail.com>
2024-06-01 18:23:36 -0400

    Initial Commit

---
+++ HealthDataCollector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthDataCollector
+//
+//  Created by Brian K Fung on 6/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
